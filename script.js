90e98d9d96f14ed3dafbdfae7f0a62e6014c88ba
crankyshorts <51765840+crankyshorts@users.noreply.github.com>
2023-06-10 23:42:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+var updates = [];
+
+function getUpdates() {
+  // Get the latest updates from the web's most visited sites.
+  // ...
+
+  // Add the updates to the `updates` array.
+  updates = updates.concat(newUpdates);
+}
+
+function updatePage() {
+  // Clear the `updates` div.
+  document.getElementById("updates").innerHTML = "";
+
+  // Add each update to the `updates` div.
+  for (var i = 0; i < updates.length; i++) {
+    var update = updates[i];
+    var li = document.createElement("li");
+    var iframe = document.createElement("iframe");
+    iframe.src = update;
+    li.appendChild(iframe);
+    document.getElementById("updates").appendChild(li);
+  }
+}
+
+setInterval(getUpdates, 10000); // Update the page every 10 seconds.
+updatePage(); // Initial update.
